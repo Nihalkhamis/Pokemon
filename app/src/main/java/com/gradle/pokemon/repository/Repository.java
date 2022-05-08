d832bfb1da0fee6912d8cd8ecbbf2c92a889a08d
Nihal <nihalyasser23@gmail.com>
2022-05-08 15:26:08 +0200

    done:room in repo and viewModel

--- app/src/main/java/com/gradle/pokemon/repository/Repository.java
+++ app/src/main/java/com/gradle/pokemon/repository/Repository.java
@@ -1,22 +1,41 @@
 package com.gradle.pokemon.repository;
 
+import com.gradle.pokemon.db.PokemonDao;
+import com.gradle.pokemon.model.Pokemon;
 import com.gradle.pokemon.model.PokemonResponse;
 import com.gradle.pokemon.network.PokemonApiService;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
+import androidx.lifecycle.LiveData;
 import io.reactivex.rxjava3.core.Observable;
 
 
 public class Repository {
     private PokemonApiService pokemonApiService;   //pokemonApiService will be provided by dagger hilt via RetrofitModule
+    private PokemonDao pokemonDao;
 
     @Inject
-    public Repository(PokemonApiService pokemonApiService) {
+    public Repository(PokemonApiService pokemonApiService, PokemonDao pokemonDao) {
         this.pokemonApiService = pokemonApiService;
+        this.pokemonDao = pokemonDao;
     }
 
     public Observable<PokemonResponse> getPokemons(){
         return pokemonApiService.getPokemons();
+    }
+
+    public void insertPokemon(Pokemon pokemon){
+        pokemonDao.insertPokemon(pokemon);
+    }
+
+    public void deletePokemon(String pokemonName){
+        pokemonDao.deletePokemon(pokemonName);
+    }
+
+    public LiveData<List<Pokemon>> getFavPokemons(){
+       return pokemonDao.getStoredPokemons();
     }
 }
--- app/src/main/java/com/gradle/pokemon/viewmodel/PokemonViewModel.java
+++ app/src/main/java/com/gradle/pokemon/viewmodel/PokemonViewModel.java
@@ -9,9 +9,11 @@ import com.gradle.pokemon.repository.Repository;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 import dagger.hilt.android.lifecycle.HiltViewModel;
@@ -21,9 +23,10 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 
 @HiltViewModel
 public class PokemonViewModel extends ViewModel {
-    private Repository repository;
 
-    MutableLiveData<ArrayList<Pokemon>> pokemonList = new MutableLiveData<>();
+    private Repository repository;
+    private MutableLiveData<ArrayList<Pokemon>> pokemonList = new MutableLiveData<>();
+    private LiveData<List<Pokemon>> favPokemonList = null;
 
     @Inject
     public PokemonViewModel(Repository repository) {
@@ -35,25 +38,38 @@ public class PokemonViewModel extends ViewModel {
     }
 
     @SuppressLint("CheckResult")
-    public void getPokemons(){
+    public void getPokemons() {
         repository.getPokemons()
                 .subscribeOn(Schedulers.io())
                 .map(new Function<PokemonResponse, ArrayList<Pokemon>>() {
                     @Override
                     public ArrayList<Pokemon> apply(PokemonResponse pokemonResponse) throws Throwable {
                         ArrayList<Pokemon> pokemonsResult = pokemonResponse.getPokemonsResult();
-                        for (Pokemon pokemon : pokemonsResult){
+                        for (Pokemon pokemon : pokemonsResult) {
                             String url = pokemon.getUrl();
                             String[] pokemonIndex = url.split("/");
-                            pokemon.setUrl("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+pokemonIndex[pokemonIndex.length-1]+".png");
+                            pokemon.setUrl("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + pokemonIndex[pokemonIndex.length - 1] + ".png");
                         }
-                        Log.d("TAG", "apply: VIEW MODEL name: "+pokemonsResult.get(0).getName());
-                        Log.d("TAG", "apply: VIEW MODEL image: "+pokemonsResult.get(0).getUrl());
+                        Log.d("TAG", "apply: VIEW MODEL name: " + pokemonsResult.get(0).getName());
+                        Log.d("TAG", "apply: VIEW MODEL image: " + pokemonsResult.get(0).getUrl());
                         return pokemonsResult;
                     }
                 })
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(result->pokemonList.setValue(result),
-                        error-> Log.d("TAG", "getPokemons: "+error.getMessage()));
+                .subscribe(result -> pokemonList.setValue(result),
+                        error -> Log.d("TAG", "getPokemons: " + error.getMessage()));
     }
+
+    public void insertPokemon(Pokemon pokemon) {
+        repository.insertPokemon(pokemon);
+    }
+
+    public void deletePokemon(String pokemonName) {
+        repository.deletePokemon(pokemonName);
+    }
+
+    public void getFavPokemons() {
+        favPokemonList = repository.getFavPokemons();
+    }
+
 }
